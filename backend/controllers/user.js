ef83c2a0836c514cfd693d8a77793c529d78bacd
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-05-11 13:41:20 +0200

    feat: errors managing

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,14 +4,22 @@ const jwt = require('jsonwebtoken')
 
 exports.register = (req, res) => {
     // Hash the password
-    hashedPassword = bcrypt.hashSync(req.body.password, 10)
+    req.body.password ? hashedPassword = bcrypt.hashSync(req.body.password, 10) : res.status(400).json({ message: 'Invalid password!' })
     var sql = `INSERT INTO users(name, email, password) VALUES ?`
     const values = [[req.body.name, req.body.email, hashedPassword],]
     db.query(sql, [values], (err) => {
         if (err) {
-            return res.status(500).json({ "error": { code: err.code, message: err.sqlMessage } })
+            switch (err.code) {
+                case 'ER_BAD_NULL_ERROR':
+                    return res.status(400).json({ message: 'All fields are required!' })
+                case 'ER_DUP_ENTRY':
+                    return res.status(409).json({ message: 'User already exists!' })
+                default:
+                    return res.status(500).json({ "error": { code: err.code, message: err.sqlMessage } })
+            }
+        } else {
+            res.status(201).json({ message: 'User created!' })
         }
-        res.status(201).json({ message: 'User created!' })
     })
 }
 
