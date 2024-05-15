791528b75576bce5eb999f8f7cb0b712b9ac9c6d
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-05-15 11:21:05 +0200

    fix: deletes nextjs app

--- frontend/src/app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
-    </main>
-  );
-}
