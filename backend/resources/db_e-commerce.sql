f7f5264f98c65c00c21e0c88b92affbe340b500a
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-05-16 11:29:32 +0200

    fix: add order_items and users

--- backend/resources/db_e-commerce.sql
+++ backend/resources/db_e-commerce.sql
@@ -6,7 +6,8 @@ USE `db_e-commerce`;
 
 CREATE TABLE `users` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
+    `firstname` varchar(255) NOT NULL,
+    `lastname` varchar(255) NOT NULL,
     `email` varchar(255) NOT NULL UNIQUE,
     `password` varchar(255) NOT NULL,
     `role` enum('admin', 'user') NOT NULL DEFAULT 'user',
@@ -15,14 +16,22 @@ CREATE TABLE `users` (
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
+CREATE TABLE `product_types` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
 CREATE TABLE `products` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
     `description` text NOT NULL,
+    `type_id` int(11) NOT NULL,
     `price` decimal(10, 2) NOT NULL,
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`type_id`) REFERENCES `product_types` (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 CREATE TABLE `orders` (
@@ -35,12 +44,11 @@ CREATE TABLE `orders` (
     FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-CREATE TABLE `order_items` (
+CREATE TABLE `order_products` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `order_id` int(11) NOT NULL,
     `product_id` int(11) NOT NULL,
     `quantity` int(11) NOT NULL,
-    `price` decimal(10, 2) NOT NULL,
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
