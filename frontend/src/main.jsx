2f43ed853a0db9a1764abe0e2e23708ae8d9142d
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-05-18 15:43:15 +0200

    feat: login page connected to the api

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,7 @@ import {
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import Root from './routes/root';
+import Login from './pages/Login';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +24,7 @@ const router = createBrowserRouter(
       <Route index element={<Navigate to='home' />} />
       <Route path="home" element={<Home />} />
       <Route path="shop" element={<Shop />} />
+      <Route path="login" element={<Login />} />
     </Route>
   )
 );
