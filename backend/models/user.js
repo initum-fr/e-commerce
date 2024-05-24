be51b526e52db923b7ebf9b517f4d5118b28820b
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-05-24 17:13:43 +0200

    feat: user model

---
+++ backend/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userModel = mongoose.Schema({
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    admin: { type: Boolean, default: false }
+})
+
+userModel.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userModel);
