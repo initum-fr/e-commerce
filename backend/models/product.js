6362a8888bc08ea3d429d9c147876c2411967d05
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-05-26 16:47:37 +0200

    feat: creates the model product

---
+++ backend/models/product.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const productModel = mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    inStock: { type: Number, required: true },
+    image: { type: String, required: true },
+    category: { type: String, default: 'Others' }
+})
+
+module.exports = mongoose.model('Product', productModel);
