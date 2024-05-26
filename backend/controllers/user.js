22d9ec93e06f714b138c7f31d843dae0d5afc995
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-05-26 16:48:25 +0200

    fix: uses mongoose for the controller user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,62 +1,29 @@
-const db = require('../config/db');
 const bcrypt = require('bcrypt');
+const User = require('../models/user')
 
 exports.getAllUsers = (req, res) => {
-    var sql = `SELECT id, firstname, lastname, role,  email, created_at, updated_at FROM users`
-    db.query(sql, (err, result) => {
-        if (err) {
-            return res.status(500).json({ "error": { code: err.code, message: err.sqlMessage } })
-        }
-        res.status(200).json(result)
-    })
+    User.find()
+        .then(users => res.status(200).json(users))
+        .catch(error => res.status(400).json({ error }))
 }
 
 exports.getOneUser = (req, res) => {
-    var sql = `SELECT id, firstname, lastname, email, role, created_at, updated_at FROM users WHERE id = ?`
-    db.query(sql, [req.params.id], (err, result) => {
-        if (err) {
-            return res.status(500).json({ "error": { code: err.code, message: err.sqlMessage } })
-        }
-        if (result.length == 0) {
-            return res.status(404).json({ message: 'User not found!' })
-        }
-        res.status(200).json(result[0])
-    })
+    User.findOne({ _id: req.params.id })
+        .then(user => res.status(200).json(user))
+        .catch(error => res.status(404).json({ error }))
 }
 
 exports.updateUser = (req, res) => {
-    console.log("here :", req.body.password)
-    let sql = ``
-    if (req.body.password != "" && req.body.password != undefined) {
-        console.log('i detected a password')
-        hashedPassword = bcrypt.hashSync(req.body.password, 10)
-        sql = `UPDATE users SET firstname = ?, lastname = ?, email = ?, password = '${hashedPassword}' WHERE id = ?`
-    } else {
-        console.log('i does not detected a password')
-        sql = `UPDATE users SET firstname = ?, lastname = ?, email = ? WHERE id = ?`
+    if (req.body.password) {
+        req.body.password = bcrypt.hashSync(req.body.password, 10)
     }
-    const values = [req.body.firstname, req.body.lastname, req.body.email, req.params.id]
-    db.query(sql, values, (err, result) => {
-        if (err) {
-            return res.status(500).json({ "error": { code: err.code, message: err.sqlMessage } })
-        }
-        res.status(200).json({ message: 'User updated!' })
-    })
+    User.updateOne({ _id: req.params.id }, { ...req.body })
+        .then(() => res.status(200).json({ message: 'User updated!' }))
+        .catch(error => res.status(400).json({ error }))
 }
 
 exports.deleteUser = (req, res) => {
-    var sql = `SELECT * FROM users WHERE id = ?`
-    db.query(sql, [req.params.id], (err, result) => {
-        if (result.length > 0) {
-            var sql = `DELETE FROM users WHERE id = ?`
-            db.query(sql, [req.params.id], (err) => {
-                if (err) {
-                    return res.status(500).json({ "error": { code: err.code, message: err.sqlMessage } })
-                }
-                return res.status(200).json({ message: 'User deleted!' })
-            })
-        } else {
-            return res.status(404).json({ message: 'User not found!' })
-        }
-    })
+    User.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'User deleted!' }))
+        .catch(error => res.status(400).json({ error }))
 }
