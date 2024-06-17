409a20532f0a9314fc7038f47f2e17973b82856a
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-06-17 16:48:57 +0200

    feat: implements react-hook-form

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,16 +1,27 @@
 import axios from 'axios'
 import { Link, useNavigate } from 'react-router-dom'
 import useSignIn from 'react-auth-kit/hooks/useSignIn'
+import { useForm } from 'react-hook-form'
+import { ErrorMessage } from '@hookform/error-message'
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
+import Modal from '../../components/Modal'
+import { useState } from 'react'
 
 export default function Login() {
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [error, setError] = useState({})
     const signIn = useSignIn()
     const navigate = useNavigate()
-    const onLogin = (event) => {
-        event.preventDefault()
-        const formData = new FormData(event.target)
-        const userData = Object.fromEntries(formData)
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm({
+        criteriaMode: 'all',
+    })
+    const onLogin = (data) => {
         axios
-            .post('http://localhost:8000/auth/login', userData)
+            .post('http://localhost:8000/auth/login', data)
             .then((response) => {
                 if (response.status === 200) {
                     if (
@@ -34,18 +45,28 @@ export default function Login() {
                             navigate('/profile', { replace: true })
                         }
                     } else {
-                        document.getElementById('alert-container').innerHTML = `
-                        <p class="text-center text-red-500 text-xs font-bold">Please enter a valid username and password.</p>`
+                        setError({
+                            title: 'Wrong username or password',
+                            message:
+                                'Please enter a valid username and password.',
+                            type: 'error',
+                        })
+                        setIsModalOpen(true)
                     }
                 }
             })
             .catch(() => {
-                document.getElementById('alert-container').innerHTML = `
-                <p class="text-center text-red-500 text-xs font-bold">Please enter a valid username and password.</p>`
+                setError({
+                    title: 'Wrong username or password',
+                    message: 'Please enter a valid username and password.',
+                    type: 'error',
+                })
+                setIsModalOpen(true)
             })
     }
     return (
         <>
+            <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen} {...error} />
             <div className="flex min-h-full flex-1 flex-col justify-center">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     {/* <img
@@ -61,9 +82,7 @@ export default function Login() {
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form
                         className="space-y-6"
-                        action="#"
-                        method="POST"
-                        onSubmit={(e) => onLogin(e)}
+                        onSubmit={handleSubmit(onLogin)}
                     >
                         <div>
                             <label
@@ -74,14 +93,38 @@ export default function Login() {
                             </label>
                             <div className="mt-2">
                                 <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    autoComplete="email"
-                                    required
+                                    {...register('email', {
+                                        required:
+                                            'Please enter your email address',
+                                        pattern: {
+                                            value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
+                                            message:
+                                                'Please enter a valid email address',
+                                        },
+                                    })}
                                     className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
+                            <ErrorMessage
+                                errors={errors}
+                                name="email"
+                                render={({ messages }) => {
+                                    console.log('messages', messages)
+                                    return messages
+                                        ? Object.entries(messages).map(
+                                              ([type, message]) => (
+                                                  <p
+                                                      key={type}
+                                                      className="mt-2 flex text-sm font-medium leading-6 text-red-700"
+                                                  >
+                                                      <ExclamationTriangleIcon className="mr-2 size-6" />
+                                                      {message}
+                                                  </p>
+                                              )
+                                          )
+                                        : null
+                                }}
+                            />
                         </div>
 
                         <div>
@@ -95,7 +138,7 @@ export default function Login() {
                                 <div className="text-sm">
                                     <a
                                         href="#"
-                                        className="font-semibold text-indigo-600 hover:text-indigo-500"
+                                        className="font-semibold text-gray-600 hover:text-gray-500"
                                     >
                                         Forgot password?
                                     </a>
@@ -103,12 +146,32 @@ export default function Login() {
                             </div>
                             <div className="mt-2">
                                 <input
-                                    id="password"
                                     name="password"
                                     type="password"
-                                    autoComplete="current-password"
-                                    required
+                                    {...register('password', {
+                                        required: 'Please enter your password',
+                                    })}
                                     className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                                <ErrorMessage
+                                    errors={errors}
+                                    name="password"
+                                    render={({ messages }) => {
+                                        console.log('messages', messages)
+                                        return messages
+                                            ? Object.entries(messages).map(
+                                                  ([type, message]) => (
+                                                      <p
+                                                          key={type}
+                                                          className="mt-2 flex text-sm font-medium leading-6 text-red-700"
+                                                      >
+                                                          <ExclamationTriangleIcon className="mr-2 size-6" />
+                                                          {message}
+                                                      </p>
+                                                  )
+                                              )
+                                            : null
+                                    }}
                                 />
                             </div>
                         </div>
@@ -116,7 +179,7 @@ export default function Login() {
                         <div>
                             <button
                                 type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 Sign in
                             </button>
@@ -127,7 +190,7 @@ export default function Login() {
                         Not a member?{' '}
                         <Link
                             to="/register"
-                            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                            className="font-semibold leading-6 text-gray-600 hover:text-gray-500"
                         >
                             Sign up !
                         </Link>
