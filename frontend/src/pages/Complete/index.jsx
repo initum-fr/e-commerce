2fbc4530212a34cf5861a6fa1121c755ff9a32d1
TheYassAnz <yassanz.contact@gmail.com>
2025-01-17 11:30:49 +0100

    fix: payment status

--- frontend/src/pages/Complete/index.jsx
+++ frontend/src/pages/Complete/index.jsx
@@ -1,7 +1,32 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+
+const STATUS_CONTENT_MAP = {
+    succeeded: {
+        text: 'Payment succeeded',
+        iconColor: '#30B130',
+        // icon: SuccessIcon,
+    },
+    processing: {
+        text: 'Your payment is processing.',
+        iconColor: '#6D6E78',
+        // icon: InfoIcon,
+    },
+    requires_payment_method: {
+        text: 'Your payment was not successful, please try again.',
+        iconColor: '#DF1B41',
+        // icon: ErrorIcon,
+    },
+    default: {
+        text: 'Something went wrong, please try again.',
+        iconColor: '#DF1B41',
+        // icon: ErrorIcon,
+    },
+}
 
 export default function Complete({ stripePromise }) {
-    const [messageBody, setMessageBody] = useState('')
+    const [status, setStatus] = useState('default')
+    const [intentId, setIntentId] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         if (!stripePromise) {
@@ -13,37 +38,42 @@ export default function Complete({ stripePromise }) {
             const clientSecret = url.searchParams.get(
                 'payment_intent_client_secret'
             )
-            const { error, paymentIntent } =
-                await stripe.retrievePaymentIntent(clientSecret)
+            // const { error, paymentIntent } =
+            //     await stripe.retrievePaymentIntent(clientSecret)
+            stripe
+                .retrievePaymentIntent(clientSecret)
+                .then(({ error, paymentIntent }) => {
+                    if (error) {
+                        setStatus('default')
+                        setIsLoading(false)
+                        return
+                    }
 
-            setMessageBody(
-                error ? (
-                    `> ${error.message}`
-                ) : (
-                    <>
-                        &gt; Payment {paymentIntent.status}:{' '}
-                        <a
-                            href={`https://dashboard.stripe.com/test/payments/${paymentIntent.id}`}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            {paymentIntent.id}
-                        </a>
-                    </>
-                )
-            )
+                    setIntentId(paymentIntent.id)
+                    setStatus(paymentIntent.status)
+                    setIsLoading(false)
+                })
         })
     }, [stripePromise])
+
     return (
         <>
-            <h1>Thank you!</h1>
-            <a href="/">home</a>
-            <div
-                id="messages"
-                role="alert"
-                style={messageBody ? { display: 'block' } : {}}
-            >
-                {messageBody}
+            <div>
+                <h1>Payment Status</h1>
+
+                <div>
+                    {isLoading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        <div>
+                            <p>
+                                {STATUS_CONTENT_MAP[status]?.text ||
+                                    STATUS_CONTENT_MAP.default.text}
+                            </p>
+                            <p>Payment ID: {intentId}</p>
+                        </div>
+                    )}
+                </div>
             </div>
         </>
     )
