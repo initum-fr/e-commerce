5d416870411005d5a4e2f8311333312417265286
TheYassAnz <yassanz.contact@gmail.com>
2025-01-17 16:21:50 +0100

    fix: /admin/users was doing multiple api call

--- frontend/src/pages/AdminUsers/index.jsx
+++ frontend/src/pages/AdminUsers/index.jsx
@@ -31,7 +31,7 @@ export default function AdminUsers() {
             .catch((error) => {
                 alert('Error: ' + error.response.data.message)
             })
-    })
+    }, [])
 
     const onDelete = (user) => {
         axios
